cf322e905b1b9c47d45ad154840f016d2848ba72
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-25 13:20:34 -0500

    hash/salt, bcrypt, routes working. Basic auth running

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('./models/User')
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require('./routes/authRoutes')
+const bodyParser = require('body-parser')
+const requireAuth = require('./middleware/requireAuth')
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(authRoutes)
+
+const mongoUri =
+  "mongodb+srv://mike:mikemike@cluster0-rlwuj.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+})
+
+mongoose.connection.on('error', (error) => {
+console.error('Error connecting to mongo', error)
+})
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
